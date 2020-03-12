628aa064df6290612c41bde19d8f3f845701dc7b
lf <332373135@qq.com>
2020-03-12 16:54:14 +0800

    注册页面异步请求前的前端逻辑，注册用户添加到数据库中的功能

---
+++ src/main/java/cn/itcast/travel/web/servlet/RegistUserServlet.java
@@ -0,0 +1,59 @@
+package cn.itcast.travel.web.servlet;
+
+import cn.itcast.travel.domain.ResultInfo;
+import cn.itcast.travel.domain.User;
+import cn.itcast.travel.service.UserService;
+import cn.itcast.travel.service.impl.UserServiceImpl;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.apache.commons.beanutils.BeanUtils;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.lang.reflect.InvocationTargetException;
+import java.util.Map;
+
+@WebServlet("/registUserServlet")
+public class RegistUserServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
+        // 1.获取数据
+        Map<String, String[]> map = request.getParameterMap();
+        // 2.封装对象
+        User user = new User();
+        try {
+            BeanUtils.populate(user, map);
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (InvocationTargetException e) {
+            e.printStackTrace();
+        }
+        // 3.调用service的regist接受user对象完成注册操作
+        UserService service = new UserServiceImpl();
+        boolean flag = service.regist(user);
+        //ResultInfo：一个处理服务端返回给客户端信息和数据的实体类，在domain包中
+        ResultInfo info = new ResultInfo();
+        // 4.响应结果
+        if (flag) {
+            // 注册成功
+            info.setFlag(true);
+        } else {
+            // 注册失败
+            info.setFlag(false);
+            info.setErrorMsg("注册失败！");
+        }
+        // 将info对象序列化为json:jackson方法
+        ObjectMapper mapper = new ObjectMapper();
+        String json = mapper.writeValueAsString(info);
+        // 将json数据写回客户端
+        // 设置content-type
+        response.setContentType("application/json;charset=utf-8");
+        response.getWriter().write(json);
+
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, java.io.IOException {
+        this.doPost(request, response);
+    }
+}
